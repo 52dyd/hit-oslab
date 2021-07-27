cb18ef09191e44fe1188eafaa241746171e73dc4
Yu.Wz <1836520635@qq.com>
2021-07-27 20:27:03 +0800

    exp.1 completed!

--- common/linux-0.11/boot/bootsect.s
+++ common/linux-0.11/boot/bootsect.s
@@ -4,27 +4,39 @@ _start:
 	mov	ax, #0x07c0
 	mov	ds, ax
 	mov	es, ax
-	mov	ss, ax
 
 	mov	ah, #0x03
 	xor	bh, bh
 	int	0x10
 
-	mov	cx, #0x10
+	mov	cx, #0x18
 	mov	bx, #0x0007
 	mov	bp, #msg
 	mov	ax, #0x1301
 	int	0x10
+
+read_setup:
+	mov	ax, #0x0201
+	mov	cx, #0x0002
+	mov	bx, #0x0200
+	mov	dx, #0x0000
+	int	0x13
+	jnc	roll
+	mov	dx, #0x0000
+	mov	ax, #0x0000
+	int	0x13
+	j	read_setup
+
 roll:
-	j	roll
+	mov	ax, #0x7e00
+	jmp	ax
 
 msg:
 	.byte 13,10
-	.ascii "hahahahaha"
+	.ascii "Loading Setup..."
 	.byte 13,10,13,10
 
 .org	510
 boot_flag:
 	.word	0xAA55
 
-.text
